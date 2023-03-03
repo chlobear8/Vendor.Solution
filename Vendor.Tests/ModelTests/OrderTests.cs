976a8fe91d06643fa78b3af1b020ee53f9027f80
Chloe ONeil <chloesporri@yahoo.com>
2023-03-03 14:53:40 -0800

    adjust tests to pass

--- Vendor.Tests/ModelTests/OrderTests.cs
+++ Vendor.Tests/ModelTests/OrderTests.cs
@@ -16,27 +16,27 @@ namespace Vendor.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Orders newOrder = new Orders("Five Pastries");
+      Orders newOrder = new Orders("Five Pastries", "Description", "Price", "Date");
       Assert.AreEqual(typeof(Orders), newOrder.GetType());
     }
 
     [TestMethod]
-    public void GetOrderName_ReturnsName_String()
+    public void GetOrderTitle_ReturnsTitle_String()
     {
-      string OrderName = "Five Pastries";
-      Orders newOrder = new Orders(OrderName);
-      string result = newOrder.OrderName;
-      Assert.AreEqual(OrderName, result);
+      string OrderTitle = "Five Pastries";
+      Orders newOrder = new Orders(OrderTitle, "Description", "Price", "Date");
+      string result = newOrder.OrderTitle;
+      Assert.AreEqual(OrderTitle, result);
     }
 
     [TestMethod]
-    public void SetOrderName_SetOrderName_String()
+    public void SetOrderTitle_SetOrderTitle_String()
     {
-      string OrderName = "Five Pastries";
-      Orders newOrder = new Orders(OrderName);
+      string OrderTitle = "Five Pastries";
+      Orders newOrder = new Orders(OrderTitle, "Description", "Price", "Date");
       string updatedOrder = "3 Bread";
-      newOrder.OrderName = updatedOrder;
-      string result = newOrder.OrderName;
+      newOrder.OrderTitle = updatedOrder;
+      string result = newOrder.OrderTitle;
       Assert.AreEqual(updatedOrder, result);
     }
 
@@ -49,12 +49,12 @@ namespace Vendor.Tests
     }
 
     [TestMethod]
-    public void GetAll_ReturnsOrderNames_OrdersList()
+    public void GetAll_ReturnsOrderTitles_OrdersList()
     {
-      string OrderName1 = "Five Pastries";
-      string OrderName2 = "3 Bread";
-      Orders newOrder1 = new Orders(OrderName1);
-      Orders newOrder2 = new Orders(OrderName2);
+      string OrderTitle1 = "Five Pastries";
+      string OrderTitle2 = "3 Bread";
+      Orders newOrder1 = new Orders(OrderTitle1, "Description", "Price", "Date");
+      Orders newOrder2 = new Orders(OrderTitle2, "Description", "Price", "Date");
       List<Orders> newList = new List<Orders> { newOrder1, newOrder2 };
       List<Orders> result = Orders.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -63,8 +63,8 @@ namespace Vendor.Tests
     [TestMethod]
     public void GetId_OrdersInitiateWithAnIdAndGetterReturns_Int()
     {
-      string OrderName = "Five Pastries";
-      Orders newOrder = new Orders(OrderName);
+      string OrderTitle = "Five Pastries";
+      Orders newOrder = new Orders(OrderTitle, "Description", "Price", "Date");
       int result = newOrder.Id;
       Assert.AreEqual(1, result);
     }
@@ -72,12 +72,13 @@ namespace Vendor.Tests
     [TestMethod]
     public void Find_ReturnsCorrentOrder_Order()
     {
-      string OrderName1 = "Five Pastries";
-      string OrderName2 = "3 Bread";
-      Orders newOrder1 = new Orders(OrderName1);
-      Orders newOrder2 = new Orders(OrderName2);
+      string OrderTitle1 = "Five Pastries";
+      string OrderTitle2 = "3 Bread";
+      Orders newOrder1 = new Orders(OrderTitle1, "Description", "Price", "Date");
+      Orders newOrder2 = new Orders(OrderTitle2, "Description", "Price", "Date");
       Orders result = Orders.Find(2);
       Assert.AreEqual(newOrder2, result);
+    
     }
   }
 }
--- Vendor.Tests/ModelTests/VendorTests.cs
+++ Vendor.Tests/ModelTests/VendorTests.cs
@@ -16,7 +16,7 @@ namespace Vendor.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendors newVendor = new Vendors("test vendor");
+      Vendors newVendor = new Vendors("test vendor", "test description");
       Assert.AreEqual(typeof(Vendors), newVendor.GetType());
     }
 
@@ -24,7 +24,7 @@ namespace Vendor.Tests
     public void GetName_ReturnsName_String()
     {
       string name= "Test Vendor";
-      Vendors newVendor = new Vendors(name);
+      Vendors newVendor = new Vendors(name, "test desciption");
       string result = newVendor.Name;
       Assert.AreEqual(name, result);
     }
@@ -33,7 +33,7 @@ namespace Vendor.Tests
     public void GetId_ReturnsVendorId_Int()
     {
       string name = "Test Vendor";
-      Vendors newVendor = new Vendors(name);
+      Vendors newVendor = new Vendors(name, "test description");
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
@@ -43,8 +43,8 @@ namespace Vendor.Tests
     {
       string vendor1 = "Suzie's Cafe";
       string vendor2 = "Trader Joe's";
-      Vendors newVendor1 = new Vendors(vendor1);
-      Vendors newVendor2 = new Vendors(vendor2);
+      Vendors newVendor1 = new Vendors(vendor1, "description");
+      Vendors newVendor2 = new Vendors(vendor2, "description");
       List<Vendors> newList = new List<Vendors> { newVendor1, newVendor2 };
       List<Vendors> result = Vendors.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -55,8 +55,8 @@ namespace Vendor.Tests
     {
       string vendor1 = "Suzie's Cafe";
       string vendor2 = "Trader Joe's";
-      Vendors newVendor1 = new Vendors(vendor1);
-      Vendors newVendor2 = new Vendors(vendor2);
+      Vendors newVendor1 = new Vendors(vendor1, "description");
+      Vendors newVendor2 = new Vendors(vendor2, "description");
       Vendors result = Vendors.Find(2);
       Assert.AreEqual(newVendor2, result);
     }
@@ -65,10 +65,10 @@ namespace Vendor.Tests
     public void AddItem_AssociatesItemWithCategory_ItemList()
     {
       string orderName = "5 Pastries";
-      Orders newOrder = new Orders(orderName);
+      Orders newOrder = new Orders(orderName, "Description", "Price", "Date");
       List<Orders> newList = new List<Orders> { newOrder };
       string name = "Suzie's Cafe";
-      Vendors newVendor = new Vendors(name);
+      Vendors newVendor = new Vendors(name, "description");
       newVendor.AddOrder(newOrder);
       List<Orders> result = newVendor.Orders;
       CollectionAssert.AreEqual(newList, result);
